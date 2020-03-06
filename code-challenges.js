e6d47ba1711b349858840dae18f8cd96f5b015b0
sjproctor <sproctor950@gmail.com>
2020-03-06 13:15:14 -0800

    fixes spacing

--- code-challenges.js
+++ code-challenges.js
@@ -2,20 +2,16 @@
 
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
-
-
-
-
 var secretCodeWord1 = "lackadaisical"
 // Expected output: "l4ck4d41s1c4l"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
+
+
+
+
 // --------------------2) Create a function that takes in an array and returns all the words that have the letter a in them.
-
-
-
-
 
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
@@ -25,10 +21,6 @@ var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is one pair and one three of a kind.
-
-
-
-
 
 var hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
